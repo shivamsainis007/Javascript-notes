1c9ecec04c60c43ddd03ba5624945008ee3b03af
shivamsainis007 <shivamsainis138@gmail.com>
2025-06-15 13:52:31 +0530

    comparison file

---
+++ comparison.js
@@ -0,0 +1,17 @@
+// console.log(2>1)
+// console.log(2>=1)
+
+
+//console.log("2">1)// java convert " 2 " into in int
+
+// console.log(null>0)  //False
+// console.log(null==0)  //false
+// console.log(null>=0)   //true
+// console.log(undefined>0)//false
+// console.log(undefined==0)//false
+// console.log(undefined>=0)//false
+
+//=== tripal eqaul  in this it will chek datatype 
+// console.log("2"===2) //flse
+// console.log("2"==2) //true
+
