dec77db071896bcff202d70e2b1b9fd6152f666e
shivamsainis007 <shivamsainis138@gmail.com>
2025-06-15 14:11:02 +0530

    more details about datatype

---
+++ dataType_summery.js
@@ -0,0 +1,27 @@
+//Primitive data type
+//  7 type data type 
+// string number boolean null  undifined symble bigInt(big for int )
+
+const score=100
+const scoreValue=100.3
+let isLogin=false
+let username        //undifined
+const id =Symbol('123')
+const anoId=Symbol('123')
+console.log(id==anoId)
+const bignum=1213124235346457567n
+
+
+// refrance type  or no primintive 
+//   Array  Objects  funtion 
+
+const hero=["skatimaan ","naagraj","doga"]
+let obj={
+    name:"shivam",
+    age:22
+
+}
+const myFuntion=function(){
+                 console.log("hello")
+}
+console.log(typeof(bignum))
