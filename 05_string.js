4045fca9a43409324d8c5fff841bcc045b7b2bc8
shivamsainis007 <shivamsainis138@gmail.com>
2025-06-18 14:25:39 +0530

    important information about for string

---
+++ 05_string.js
@@ -0,0 +1,26 @@
+// const name="parkash"
+// const repo=50
+// console.log(name+repo+"have")
+// console.log(`hello my name ${name}`)
+// const gameName=new String("hello")// with this wqy we get some other ferture like this gameName={0:h,1:e -------- and so on }
+// console.log(gameName[3])
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase());
+// console.log(gameName.substring(0,3))
+// const anatherGame=gameName.slice(-5)
+// console.log(anatherGame)
+
+// let name="    hithesh    "
+// console.log(name.trim())
+// console.log(name.trimStart())
+// console.log(name.trimEnd())
+// const url="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/valueOf"
+// console.log(url.replace("Web" ,"wen"))
+
+
+// spilt funtion
+let str="hello my name is shivam"
+newStr= str.split(" ")
+console.log(newStr)
+console.log(newStr.join('-'))
+
