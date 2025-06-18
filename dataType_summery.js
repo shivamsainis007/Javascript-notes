fc1557e7d5063643e0ce55425107595648532cdb
shivamsainis007 <shivamsainis138@gmail.com>
2025-06-18 12:36:41 +0530

    leran about heap and stach memory

--- dataType_summery.js
+++ dataType_summery.js
@@ -2,26 +2,47 @@
 //  7 type data type 
 // string number boolean null  undifined symble bigInt(big for int )
 
-const score=100
-const scoreValue=100.3
-let isLogin=false
-let username        //undifined
-const id =Symbol('123')
-const anoId=Symbol('123')
-console.log(id==anoId)
-const bignum=1213124235346457567n
+// const score=100
+// const scoreValue=100.3
+// let isLogin=false
+// let username        //undifined
+// const id =Symbol('123')
+// const anoId=Symbol('123')
+// console.log(id==anoId)
+// const bignum=1213124235346457567n
 
 
-// refrance type  or no primintive 
-//   Array  Objects  funtion 
+// // refrance type  or no primintive 
+// //   Array  Objects  funtion 
 
-const hero=["skatimaan ","naagraj","doga"]
-let obj={
-    name:"shivam",
-    age:22
+// const hero=["skatimaan ","naagraj","doga"]
+// let obj={
+//     name:"shivam",
+//     age:22
+
+// }
+// const myFuntion=function(){
+                //  console.log("hello")}
+// console.log(typeof(bignum))
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+    
+
+// how to alloct a memoy in java =script 
+// using heap(non primiritve) and stack(primitive)
+
+//  nsole.log(anotherName)
+
+
+// it hapen for becouse of that it give me copy
+// and non primitive datatype give  us real addres
+
+let user={
+    email :"shivamsaini@gmail.com",
+    name:"shivam"
 
 }
-const myFuntion=function(){
-                 console.log("hello")
-}
-console.log(typeof(bignum))
+let user2=user
+user2.name="paarskas"
+// console.log(user.name)
+// console.log(user2.name)
