1cd88092ad36f7b273325a16d55005ee4c471cf6
shivamsainis007 <shivamsainis138@gmail.com>
2025-06-19 17:30:31 +0530

    date and time

---
+++ 07_date_time.js
@@ -0,0 +1,28 @@
+////    dates
+
+// let myDate=new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString( ))
+// console.log(typeof myDate)
+
+// let Date1=new Date(2002,9,2,23,15,15,)
+// console.log(Date1.toLocaleString())
+
+// Date2=new Date("2023-01-14")
+// console.log(Date2.toLocaleString())
+// let Date3=new Date("01-14-2013")
+// console.log(Date3.toLocaleString)
+
+//     Time stamp
+// let TimeStamp=Date.now()
+// console.log(TimeStamp)
+// console.log(Date3.getTime())
+// console.log(Math.floor(TimeStamp/1000))
+
+
+//     
+let date=new Date()
+console.log(date.toLocaleString('defult',{
+    weekday:"long"
+}))
