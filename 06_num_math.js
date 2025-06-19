208c5b0702b87712b3a41146ef40288456ef5e22
shivamsainis007 <shivamsainis138@gmail.com>
2025-06-19 10:45:12 +0530

    number and math

---
+++ 06_num_math.js
@@ -0,0 +1,34 @@
+//       number
+
+
+// const score=400
+// const balance=new Number(100)
+// console.log(score)
+// console.log(balance)
+
+// const otherNumber=123.897
+// console.log(otherNumber.toPrecision(4))
+
+// const hunders=1000000
+// console.log(hunders.toLocaleString())
+
+
+
+//      Math
+
+// console.log(Math)
+// console.log(Math.abs(-4 ))
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.2))//5
+// console.log(Math.floor(4.9))//4
+// console.log(Math.min(1,2,3,4,5))//1
+// console.log(Math.max(1,2,3,4,5))//5
+
+// console.log(Math.random())
+// console.log((Math.random()*10)+1)
+ 
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+
