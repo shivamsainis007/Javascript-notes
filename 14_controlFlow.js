9bff80923b7aed60b7a182441992cd942409cae0
shivamsainis007 <shivamsainis138@gmail.com>
2025-06-28 13:04:07 +0530

    if else

---
+++ 14_controlFlow.js
@@ -0,0 +1,19 @@
+// controlflow
+// 1. if-else
+// let temperature = 30;
+// if (temperature>25) {
+//     console.log("This is true");
+// }
+// else{
+//     console.log("This is false");
+// }
+// >,<,<=,>=,==,!= conditions check  ===(type and value), !==(type and value)
+const age = 18;
+const aadarcrad= true;
+if (age >= 18 && aadarcrad) {
+    console.log("You can vote");
+}
+// or operator give me cod
+if (age >= 18 || aadarcrad) {
+    console.log("You can vote");
+}
