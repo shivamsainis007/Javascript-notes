77a4e612ec0fd5a61c6298e1b472dfbb2f094041
shivamsainis007 <shivamsainis138@gmail.com>
2025-06-30 23:03:39 +0530

    loops

---
+++ 15_loops.js
@@ -0,0 +1,68 @@
+// loops 
+// for loops
+// let arr=[1,2,3,4,5]
+// for (let index = 0; index < arr.length; index++) {
+//     const element = arr[index];
+//     console.log(`element is :${element}`);
+    
+// }
+
+// for (let i = 1; i <= 5; i++) {
+//     for (j=1; j<=5;j++){
+//         console.log(i+"*"+j+"="+i*j);
+        
+//     }
+    
+// }
+
+// brack and continue
+// for (let i = 1; i <= 5; i++) {
+//     if (i === 3) {
+//         console.log("Skipping the rest of the loop when i is 3");
+//         continue; // skip the rest of the loop when i is 3
+//     }
+//     console.log(`Current value of i: ${i}`);
+// }
+// for (let i = 1; i <= 5; i++) {
+//     if (i === 3) {
+//         console.log("Breaking the loop when i is 3");
+//         break; // exit the loop when i is 3
+//     }
+//     console.log(`Current value of i: ${i}`);
+// }
+
+// +++++++++++++++++++++++++++ while loop +++++++++++++++++++++++++++
+// let i = 1;
+// while (i <= 5) {
+//     console.log(`Current value of i: ${i}`);
+//     i++;
+// }
+
+// +++++++++++++++++++++++++++ do while loop +++++++++++++++++++++++++++
+// let i= 1;
+// do {
+//     console.log(`Current value of i: ${i}`); //it print al liest one time
+//     i++;
+// }while (i <= 5);
+
+
+// +++++++++++++++++++++++++++ for of loop +++++++++++++++++++++++++++
+// const arr = [1, 2, 3, 4, 5];
+// for (const element of arr) {
+//     console.log(`Element is: ${element}`);
+// }
+
+// const str = "Hello";
+// for (const char of str) {
+//     console.log(`Character is: ${char}`);
+// }
+
+// +++++++++++++++++++++++++++ for in loop +++++++++++++++++++++++++++
+// const obj = { a: 1, b: 2, c: 3 };
+// for (const key in obj) {
+//     if (Object.hasOwnProperty.call(obj, key)) {
+//         const value = obj[key];
+//         console.log(`Key: ${key}, Value: ${value}`);
+//     }
+// }
+
